c4d9aa01d1c70ca459aad44939dc9fde68ab4e86
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-01-27 08:42:14 +0300

    Added links for navigation

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from flask import render_template
 from app import app
-from .request import  get_movies
+from .requests import  get_movies, get_movie
+
 
 # Views
-
 @app.route('/')
 def index(): 
     '''
@@ -17,11 +17,22 @@ def index():
     return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+# @app.route('/movie/<int:movie_id>')
+# def movies(movie_id):
+
+#     '''
+#     View root page function that returns the index page and its data
+#     '''
+#     title = " Movie Shows"
+#     return render_template('movie.html', id= movie_id )
+
+@app.route('/movie/<int:id>')
+def movie(id):
 
     '''
-    View root page function that returns the index page and its data
+    View movie page function that returns the movie details page and its data
     '''
-    title = " Movie Shows"
-    return render_template('movie.html', id= movie_id )
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
