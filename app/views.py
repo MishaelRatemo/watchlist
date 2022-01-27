b5aaed883e948470741481797f38be666cd459c6
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-01-27 20:04:27 +0300

    Added WTF forms to add a review to a movie

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from flask import render_template,request, redirect, url_for
 from app import app
 from .requests import  get_movies, get_movie, search_movie
+from .models import reviews
+from .forms import ReviewForm
+Review = reviews.Review
 
 
 # Views
@@ -24,17 +27,6 @@ def index():
     
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
-
-
-# @app.route('/movie/<int:movie_id>')
-# def movies(movie_id):
-
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-#     title = " Movie Shows"
-#     return render_template('movie.html', id= movie_id )
-
 @app.route('/movie/<int:id>')
 def movie(id):
 
@@ -43,8 +35,11 @@ def movie(id):
     '''
     movie = get_movie(id)
     title = f'{movie.title}'
+    
+    # Review
+    reviews = Review.get_reviews(movie.id)
 
-    return render_template('movie.html',title = title,movie = movie)
+    return render_template('movie.html',title = title,movie = movie, reviews=reviews)
 
 @app.route('/search/<movie_name>')
 def search(movie_name):
@@ -56,3 +51,19 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
+
