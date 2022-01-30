3ffe47a03a47f4af8bb623f3b7c4eb2cbc1d5c63
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-01-30 20:50:28 +0300

    deployment

--- manage.py
+++ manage.py
@@ -1,15 +1,15 @@
 
 from app import create_app
-# from flask_script import Manager,Server
+from flask_script import Manager,Server
 
 # Creating app instance
 app = create_app('development')
 
-# manager = Manager(app)
+manager = Manager(app)
 # manager.add_command('server',Server)
 
 # from app.main import app
 
 if __name__ == '__main__':
     # app.run()
-    app.run()
+    manager.run()
