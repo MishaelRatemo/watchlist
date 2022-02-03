ad1b4051db29f71291a2c484606bee0d2971bde6
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-03 05:23:01 +0300

    CRUD using SQLAlchemy

--- manage.py
+++ manage.py
@@ -1,11 +1,18 @@
 from app import create_app
 from flask_script import Manager,Server
+from app import db
+from app.models import User
 
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 
 # from app.main import app
 
@@ -19,20 +26,12 @@ if __name__ == '__main__':
 
 
 
-from app import create_app
-from app import db
-from app.models import User
-
-# Creating app instance
-app = create_app('development')
-
-@app.shell
-def make_shell_context():
-    return dict(app = app,db = db,User = User )
 
 
-if __name__ == '__main__':
-    app.run()
-    
-    
+
+
+
+
+
+
     
