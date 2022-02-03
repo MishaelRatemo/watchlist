d5d0254f5e725f2aeed43d36bb0428a77bcbd7e6
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-03 03:34:28 +0300

    Added shell

--- manage.py
+++ manage.py
@@ -1,15 +1,18 @@
 
 from app import create_app
-# from flask_script import Manager,Server
+from app import db
+from app.models import User
 
 # Creating app instance
 app = create_app('development')
 
-# manager = Manager(app)
-# manager.add_command('server',Server)
+@app.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 
-# from app.main import app
 
 if __name__ == '__main__':
     app.run()
-    # manager.run()
+    
+    
+    
