98923f4258d164d7d7ef4149a28d217d2be5e38b
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-03 12:07:02 +0300

    added fucntions to user modelto secure passwords

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 class Movie:
     '''
@@ -53,6 +55,18 @@ class User(db.Model): # db.model arg help us to connect to DB and communicate
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     # colum to hold passwords
     pass_secure = db.Column(db.String(255))
+    
+    @property # allows only write only
+    def password(self):
+        raise AttributeError('You cannot read the password attribute') # raise blocks access to password
+
+    @password.setter
+    def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+    
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+
     
     def __repr__(self):
         return f'User {self.username}'
