05c87ae49917a76903a22550bbbdbcc5d821b845
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-04 16:49:11 +0300

    exported mail and mail-pass

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 export MOVIE_API_KEY=85459567afbba3a7094fe38bddbc8eb7
 export SECRET_KEY=secretkey
+
+export MAIL_USERNAME='mishaelfilms@gmail.com'
+export MAIL_PASSWORD='watchlistfilms'
 python3 manage.py server
