15b9ed0e1b9d92f8f1b050dba31bf0208be6dffd
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-04 16:12:37 +0300

    added mail configurations

--- config.py
+++ config.py
@@ -40,6 +40,13 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/watchlist'
     #specify the destination  where you want to store  Images.
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     pass
