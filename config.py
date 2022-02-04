a39c724a2301b6556e72d34c742b4721ff79dd96
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-04 14:02:10 +0300

    created new configs which specifies the destination to where we want to store our Images.

--- config.py
+++ config.py
@@ -38,7 +38,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/watchlist'
-
+    #specify the destination  where you want to store  Images.
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
     pass
