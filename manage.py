bc8779dd14c52b2c11de1eb2d3d97225d1d6de52
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-02-05 23:27:25 +0300

    update how we define our save review instance.

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ from flask_script import Manager,Server
 from app import db
 from app.models import User
 from app.models import Role
+from app.models import Review
 from flask_migrate import Migrate, MigrateCommand
 
 
@@ -20,7 +21,7 @@ manager.add_command('db',MigrateCommand)
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User,
-                 Role=Role)
+                 Role=Role,Review=Review)
 
 # from app.main import app
 
